25072ed80d440e86794fcdfeb2e229c2691d537a
harisura123 <surya7075303536@gmail.com>
2025-04-15 11:34:30 +0530

    Ready to Deploy

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const {connection} = require('./db')
+
+const app = express()
+
+app.use(express.json());
+app.use(cors());
+
+const AuthRouter = require('./Routes/AuthRouter');
+const CategoryRouter = require('./Routes/Categories');
+
+app.listen(process.env.PORT, async () => {
+    try{
+        await connection;
+        console.log(`Db is connect Successfully`)
+    }catch(err){
+        console.log(err)
+    }    
+    console.log(`Server is Running`)
+})
+
+app.use('/auth', AuthRouter);
+
+app.use('/api', CategoryRouter);
+
